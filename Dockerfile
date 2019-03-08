e11497546e329f0e39d7d0027e97dcdf56c48450
Tony Heckmann <anthony.heckmann@gmail.com>
2019-03-08 03:06:56 -0500

    everything is working

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM python:alpine
 WORKDIR /app
 RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev \
     && pip install psycopg2 \
-    && pip install Flask-SQLAlchemy
+    && pip install Flask-SQLAlchemy \
+    && pip install 'sentry-sdk[flask]==0.7.6'
 COPY take_home.py /app
 COPY app_entry.sh /app
 CMD ["python", "take_home.py"]
--- take_home.py
+++ take_home.py
@@ -1,94 +1,19 @@
-'''
-                     .                          
-                     M                          
-                    dM                          
-                    MMr                         
-                   4MMML                  .     
-                   MMMMM.                xf     
-   .              "MMMMM               .MM-     
-    Mh..          +MMMMMM            .MMMM      
-    .MMM.         .MMMMML.          MMMMMh      
-     )MMMh.        MMMMMM         MMMMMMM       
-      3MMMMx.     'MMMMMMf      xnMMMMMM"       
-      '*MMMMM      MMMMMM.     nMMMMMMP"        
-        *MMMMMx    "MMMMM\    .MMMMMMM=         
-         *MMMMMh   "MMMMM"   JMMMMMMP           
-           MMMMMM   3MMMM.  dMMMMMM            .
-            MMMMMM  "MMMM  .MMMMM(        .nnMP"
-=..          *MMMMx  MMM"  dMMMM"    .nnMMMMM*  
-  "MMn...     'MMMMr 'MM   MMM"   .nMMMMMMM*"   
-   "4MMMMnn..   *MMM  MM  MMP"  .dMMMMMMM""     
-     ^MMMMMMMMx.  *ML "M .M*  .MMMMMM**"        
-        *PMMMMMMhn. *x > M  .MMMM**""           
-           ""**MMMMhx/.h/ .=*"                  
-                    .3P"%....                   
-                  nP"     "*MMnx      
-
-
- /$$                      /$$$$$$  /$$ /$$           /$$            
-| $$                     /$$__  $$| $$|__/          | $$            
-| $$  /$$$$$$   /$$$$$$ | $$  \__/| $$ /$$ /$$$$$$$ | $$   /$$      
-| $$ /$$__  $$ |____  $$| $$$$    | $$| $$| $$__  $$| $$  /$$/      
-| $$| $$$$$$$$  /$$$$$$$| $$_/    | $$| $$| $$  \ $$| $$$$$$/       
-| $$| $$_____/ /$$__  $$| $$      | $$| $$| $$  | $$| $$_  $$       
-| $$|  $$$$$$$|  $$$$$$$| $$      | $$| $$| $$  | $$| $$ \  $$      
-|__/ \_/$$___/ \_______/|__/      |__/|__/|__/  |__/|__/  \__/      
-      | $$                                                          
-  /$$$$$$$  /$$$$$$  /$$    /$$ /$$$$$$   /$$$$$$   /$$$$$$$        
- /$$__  $$ /$$__  $$|  $$  /$$//$$__  $$ /$$__  $$ /$$_____/        
-| $$  | $$| $$$$$$$$ \  $$/$$/| $$  \ $$| $$  \ $$|  $$$$$$         
-| $$  | $$| $$_____/  \  $$$/ | $$  | $$| $$  | $$ \____  $$        
-|  $$$$$$$|  $$$$$$$   \  $/  |  $$$$$$/| $$$$$$$/ /$$$$$$$/        
- \_/$$___/ \_______/    \_/    \_/$$__/ | $$____/ |_______/         
-  | $$                          | $$    | $$                        
- /$$$$$$    /$$$$$$   /$$$$$$$ /$$$$$$  | $$                        
-|_  $$_/   /$$__  $$ /$$_____/|_  $$_/  |__/                        
-  | $$    | $$$$$$$$|  $$$$$$   | $$                                
-  | $$ /$$| $$_____/ \____  $$  | $$ /$$                            
-  |  $$$$/|  $$$$$$$ /$$$$$$$/  |  $$$$/                            
-   \___/   \_______/|_______/    \___/                              
-                                                                    
-
-This application won't work! Your challenge is to fix that.
-
-Please connect this application to a database
-and encapsulate it in such a way that you can send me a 
-zip of the dir and I can have this up and running if I have
-
-* Docker + docker-compose
-* Python with the following packages:
-    * Flask
-    * SqlAlchemy
-* VirtualEnv
-
-installed.
-
-How you do so is entirely up to you. I just need to be able to 
-easily run it on my machine! 
-
-You can install additional python packages (if, say, you use virtalenv) 
-but I shouldn't  need to install any additional system level packages.
-
-
-HINT: Here at LeafLink we use docker-compose 
-
-Deliverable: a zip or otherwise compressed file. This file
-MUST include take_home.py and it MUST include a README.MD
-that will explain how to run this app so it works. It can
-include as many or as little other files as you wish so long
-as they do not destroy my computer.
-
-HINT: The less I need to type to see that it works, the better
-
-                                                                                                                                                                                                     
-'''
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_SDK_URL'),
+    integrations=[FlaskIntegration()]
+)
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =  "postgresql://myuser:mypassword@db/mydb"
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 db = SQLAlchemy(app)
 
@@ -105,7 +30,11 @@ def hello():
     admin = User(username='admin', email='admin@example.com')
     db.session.add(admin)
     db.session.commit()
-    return "It works"
+    return "It works\n"
+
+@app.route("/health")
+def health():
+    return "Simple Server Working\n"
 
 db.create_all()
-app.run()
+app.run(host='0.0.0.0')
